eed45ad45f11ef679d1f0931c73f0c1d24447ca6
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-11-07 16:51:03 +0000

    add friend jsx

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import {Route} from "react-router";
-import Home from "./components/home";
+import { Route, IndexRoute } from "react-router";
+import { Home } from "./components/home";
+import { YourAwesomeComponent } from "your-awesome-published-npm-module";
 
 export const routes = (
-  <Route path="/" component={Home}/>
+  <Route path="/" component={Home}>
+    <IndexRoute component={YourAwesomeComponent}/>
+    <Route path="/invite" component={YourAwesomeComponent}/>
+  </Route>
 );
